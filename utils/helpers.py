15585e5bd5ca19336f1483cc838e61a68f667a5d
Avinash <avinashnaidu2131@gmail.com>
2026-02-14 18:39:19 +0530

    Fixed schemes.json path for Streamlit deployment

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,5 +1,9 @@
 import json
+import os
 
 def load_schemes():
-    with open("schemes.json", "r", encoding="utf-8") as f:
+    base_path = os.path.dirname(os.path.dirname(__file__))
+    file_path = os.path.join(base_path, "data", "schemes.json")
+
+    with open(file_path, "r", encoding="utf-8") as f:
         return json.load(f)
